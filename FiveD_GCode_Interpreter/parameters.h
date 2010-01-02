0393a025b6affdca31a0dbcde29a75ac1da21c5d
buzz <buzz@emc2.(none)>
2010-01-02 14:10:30 +1000

    buzzs reprap settings from previous 2D firmware

--- FiveD_GCode_Interpreter/parameters.h
+++ FiveD_GCode_Interpreter/parameters.h
@@ -78,43 +78,47 @@
 #define INCHES_TO_MM 25.4
 
 // define the parameters of our machine.
-#define X_STEPS_PER_MM   9.8482399079
+#define X_STEPS_PER_MM   3.58
 #define X_STEPS_PER_INCH (X_STEPS_PER_MM*INCHES_TO_MM)
-#define X_MOTOR_STEPS    400
+#define X_MOTOR_STEPS    400 //1.8 deg/step
 #define INVERT_X_DIR 0
 
-#define Y_STEPS_PER_MM   9.8482399079
+// buzzs belt-driven y-axis
+#define Y_STEPS_PER_MM   3.94 //82? 
 #define Y_STEPS_PER_INCH (Y_STEPS_PER_MM*INCHES_TO_MM)
 #define Y_MOTOR_STEPS    400
 #define INVERT_Y_DIR 1
 
-#define Z_STEPS_PER_MM   160 // 200 steps per revoloution / 1.25 mm thread pitch
+#define Z_STEPS_PER_MM   20 //10 = way too small, 30 = a bit too big
 #define Z_STEPS_PER_INCH (Z_STEPS_PER_MM*INCHES_TO_MM)
-#define Z_MOTOR_STEPS    200
-#define INVERT_Z_DIR 0
+#define Z_MOTOR_STEPS    400
+#define INVERT_Z_DIR 1
 
 // For when we have a stepper-driven extruder
 // E_STEPS_PER_MM is the number of steps needed to 
 // extrude 1mm out of the nozzle.
 
-#define E_STEPS_PER_MM   0.7525056 // drive diameter = 4.7 mm, material in:material out = 36:1, 400 steps per revolution
+// STEPPER:
+//#define E_STEPS_PER_MM   0.706   // 5mm diameter drive - empirically adjusted.   
+//for ENCODER DC motors, this is "encoder wheel edges per MM of extrudate"
+#define E_STEPS_PER_MM   1  //fixed at 1 for convenience sake - see below ( the steps() function is called with 'true' this often)
 #define E_STEPS_PER_INCH (E_STEPS_PER_MM*INCHES_TO_MM)
 #define E_MOTOR_STEPS    400
 
 //our maximum feedrates
-#define FAST_XY_FEEDRATE 4000.0
-#define FAST_Z_FEEDRATE  60.0
+#define FAST_XY_FEEDRATE 2000.0
+#define FAST_Z_FEEDRATE  5.0
 
 // Data for acceleration calculations
 // Comment out the next line to turn accelerations off
-//#define ACCELERATION_ON
+#define ACCELERATION_ON
 #define SLOW_XY_FEEDRATE 1000.0 // Speed from which to start accelerating
 #define SLOW_Z_FEEDRATE 20
 
 // Set to 1 if enable pins are inverting
 // For RepRap stepper boards version 1.x the enable pins are *not* inverting.
 // For RepRap stepper boards version 2.x and above the enable pins are inverting.
-#define INVERT_ENABLE_PINS 1
+#define INVERT_ENABLE_PINS 0
 
 #if INVERT_ENABLE_PINS == 1
 #define ENABLE_ON LOW
@@ -124,7 +128,7 @@
 
 // Set to one if sensor outputs inverting (ie: 1 means open, 0 means closed)
 // RepRap opto endstops are *not* inverting.
-#define ENDSTOPS_INVERTING 0
+#define ENDSTOPS_INVERTING 1
 
 // Inline interrupt control functions
 
